71816b13f73c1bcc0f3f6eea95038eaa4ca5c40f
Neeraj <neerajmilan968@gmail.com>
2024-01-07 13:20:50 +0530

    fixed port bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const app = express()
 
+const PORT = process.env.PORT
 
 app.get("/",(req,res)=>{
     res.send("<h1 style='color:green'>Hello World from Home Page!</h1>")
@@ -13,6 +14,6 @@ app.get("/hello",(req,res)=>{
 })
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server started on Port : ${PORT}`)
 })
